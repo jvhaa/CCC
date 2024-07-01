322b78c288200220bab6ab8240258365b4b79014
Sam Lee <68556120+jvhaa@users.noreply.github.com>
2024-07-01 15:32:36 -0400

    Create CCC '13 J1 - Next in line.py

---
+++ J1/CCC '13 J1 - Next in line.py
@@ -0,0 +1,9 @@
+# Canadian Computing Competition: 2013 Stage 1, Junior #1
+# https://dmoj.ca/problem/ccc13j1
+
+import sys
+
+i = int(sys.stdin.readline())
+l = int(sys.stdin.readline(2))
+
+print(l+l-i)
