958ce40f1770872e46c681d91e8f65b4056e1838
Sam Lee <68556120+jvhaa@users.noreply.github.com>
2024-07-01 15:43:19 -0400

    Create CCC '24 J1 - Conveyor Belt Sushi.py

---
+++ J1/CCC '24 J1 - Conveyor Belt Sushi.py
@@ -0,0 +1,7 @@
+# Canadian Computing Competition: 2024 Stage 1, Junior #1
+# https://dmoj.ca/problem/ccc24j1
+
+import sys
+input = sys.stdin.readline
+
+print(int(input())*3+int(input())*4+int(input())*5)
