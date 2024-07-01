72e5adb71836c844eec5839065d964e4ae4733e1
Sam Lee <68556120+jvhaa@users.noreply.github.com>
2024-07-01 15:31:15 -0400

    Update CCC '12 J1 - Speed fines are not fine.py

--- J1/CCC '12 J1 - Speed fines are not fine.py
+++ J1/CCC '12 J1 - Speed fines are not fine.py
@@ -1,1 +1,18 @@
+# Canadian Computing Competition: 2012 Stage 1, Junior #1
+# https://dmoj.ca/problem/ccc12j1
 
+import sys
+
+limit = int(sys.stdin.readlines(1)[0])
+speed = int(sys.stdin.readlines(1)[0])
+
+total = speed - limit
+
+if total > 0 and total < 21:
+    print("You are speeding and your fine is $100.")
+elif total > 20 and total < 31:
+    print("You are speeding and your fine is $270.")
+elif total > 30:
+    print("You are speeding and your fine is $500.")
+else:
+    print("Congratulations, you are within the speed limit!")
